2235c848d1cf3fb4aa9c82c02aa5cba560386a45
+ <+>
2024-05-21 23:55:57 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "Birthday"
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+  
+
+
+writeCards(cards, eventName);
+
+
+function countDown(count) {
+    while (count > -1) {
+      console.log(count);
+      count--;
+    }
+  }
+    countDown(10);
+    countDown(4);
